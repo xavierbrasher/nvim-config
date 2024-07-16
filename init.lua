c32ed4d384c5abfb1d10bfb0b0353486bddb5f36
Xavier Brasher <xavier.brasher@gmail.com>
2024-07-16 17:07:00 +0800

    Removed escape for jj only

--- init.lua
+++ init.lua
@@ -57,6 +57,7 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "<Esc>", "<Nop>")
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
