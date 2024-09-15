fbdf3f6906fb93ad7d8dcbac5c0c1ff019e4783d
xavierbrasher <xavier.brasher@gmail.com>
2024-09-15 13:18:44 +0800

    changed theme to lavish

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,9 +1,10 @@
-return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	config = true,
-	opts = function()
-		vim.o.background = "dark" -- or "light" for light mode
-		vim.cmd([[colorscheme gruvbox]])
-	end,
-}
+return {}
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	priority = 1000,
+-- 	config = true,
+-- 	opts = function()
+-- 		vim.o.background = "dark" -- or "light" for light mode
+-- 		vim.cmd([[colorscheme gruvbox]])
+-- 	end,
+-- }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -99,14 +99,6 @@ return { -- LSP Configuration & Plugins
 			clangd = {},
 			pyright = {},
 			rust_analyzer = {},
-			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
-			--
-			-- But for many setups, the LSP (`tsserver`) will work just fine
-			tsserver = {},
-			--
 
 			lua_ls = {
 				-- cmd = {...},
